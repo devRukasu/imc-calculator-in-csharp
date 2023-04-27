20794149181c9046acbddf6c742aafd7ec399c22
Lucas Richard <lucasrss68@gmail.com>
2023-04-27 11:03:40 -0300

    first commit

---
+++ CalculadoraIMC/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraIMC
+{
+    internal class Pessoa
+    {
+        public float peso;
+        public float altura;
+        public float resultado;
+        public string situacao;
+
+        public double CalcularIMC()
+        {
+            resultado = peso / (altura * altura);
+            return resultado;
+        }
+
+        public string TabelaIMC()
+        {
+            if (resultado < 18.5)
+                situacao = "Abaixo do Peso";
+            else if (resultado < 25)
+                situacao = "Peso Normal";
+            else if (resultado < 30)
+                situacao = "Acima do Peso";
+            else if (resultado < 35)
+                situacao = "Obesidade I";
+            else if (resultado < 40)
+                situacao = "Obesidade II";
+            else
+                situacao = "Obesidade III";
+
+            return situacao;
+        }
+
+        public void Mensagem()
+        {
+            Console.WriteLine($"A sua situação é {situacao}");
+        }
+    }
+}
---
+++ CalculadoraIMC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("Digite o seu peso");
+            pessoa.peso = System.Single.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a sua altura");
+            pessoa.altura = System.Single.Parse(Console.ReadLine());
+
+            pessoa.CalcularIMC();
+            pessoa.TabelaIMC();
+            pessoa.Mensagem();
+        }
+    }
+}
